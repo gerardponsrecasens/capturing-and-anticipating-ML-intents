4e268030bafa0e4b04bd20e85febb6c41b3b3b4d
gerardponsrecasens <gerardpons.10@gmail.com>
2023-04-12 19:12:45 +0200

    Added foundation for UI

---
+++ UI/app/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from os import path
+
+
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'hwdkjcn90js'
+
+    from .views import views
+
+    app.register_blueprint(views, url_prefix='/')
+
+
+    return app
+
+
