a1f757ae617428e7324b398ae1818afb54ac4b7c
Gerard Pons <gerardpons.10@gmail.com>
2025-09-29 10:47:21 +0200

    GNN experiments & Workflow change

--- README.md
+++ README.md
@@ -1,20 +1,19 @@
-# Capturing and anticipating user intents in complex analytics workflows
+# Capturing and Anticipating User Intents in Data Analytics via Knowledge Graphs
 
-This repository contains the main scripts for the work **On Enhancing AutoML Tools with Knowledge Graphs**, which correspond the Task 4.3 of the **ExtremeXP** project, focusing on two key functionalities of the ExtremeXP framework: 
+This repository contains the main scripts and Knowledge Graph for the work **Capturing and Anticipating User Intents in Data Analytics via Knowledge Graphs**, focusing on: 
 - Capturing of user intents, user preferences, and constraints in a newly designed Knowledge Graph.
-- Suggesting the end-users the different input parameters, enabling them to interact in a more efficient and intuitive manner with the system.
+- Suggesting the end-users the different input parameters, enabling them to interact in a more efficient and intuitive manner with Data Analytics Assistants system.
 
 
 To achieve it, different steps have been followed: 
 1. A Knowledge Graph has been **designed** to capture all the requiered concepts and relationships related to the data mining domain and the user interaction with the system, so that the generated knowledge graph can be used in a future step to derive valid workflows.
 2. The Knowledge Graph has been **populated** with ML workflows from various sources.
-3. Inference algorithms have been applied to the populated knowledge graph, so that it can be used to find **relevant intents, constraint and preferences** (e.g., serve as a reccomender system).
+3. **Experiments** have been conducted to assess how well KGE and GNN can capture the KG's structure to provide recommendations with Link Prediction, and how sensible are these recommendations. 
+4. Inference algorithms have been applied to the populated knowledge graph in a prototype UI, so that it can be used to find **relevant intents, constraint and preferences** (e.g., serve as a reccomender system).
 
 
 
-
-
-![GeneralFlowIDA](https://github.com/gerardponsrecasens/capturing-and-anticipating-ML-intents/assets/95172600/c118b0e4-1122-4560-bae4-5428e7a121ef)
+![alt text](GeneralFlowIDA.png)
 
 
 The repository is organized as follows: in the *Knowledge Graph* folder, the ontology .nt files can be found, alongside with the scripts used to generate them. Then, in the ontology *Knowledge Graph* folder the scripts for creating the instances from the different sources can be found. Then, the *Recommender* folder contains the scripts used to experiment with the anticipation system. Finally, in the *UI* folder, the script to execute a prototype of the whole system can be found. A video demo of the prototype can be found [here](https://youtu.be/MMHCoE6yonw).
--- recommender/experiments.py
+++ recommender/experiments.py
@@ -5,72 +5,134 @@ import torch
 
 
 # Indicate if in evaluation the relationships should be filtered
-filtered_evaluation = True
+filtered_evaluation = False
 evaluation_relation_whitelist = {'<http://localhost/8080/intentOntology#hasConstraint>', 
                                  '<http://localhost/8080/intentOntology#hasIntent>'}
 
 # Load dataset
-tf = TriplesFactory.from_path(r'all_clean.tsv')
+tf = TriplesFactory.from_path(r'data.tsv')
 training, testing, validation = tf.split([.8, .1, .1], random_state = 1998)
 
 
 epochs = 300
-models = ['TransE','TransH','DistMult','ComplEx','RotatE','TransR']
-emb_dimensions = [2,8,16,32,64]
-learning_rates = [0.001,0.01]
-num_negatives = [1,10,20]
+models = ['TransE','TransH','DistMult','ComplEx','RotatE','TransR','RGCN']
+seeds = [11,22,33,44,55]
+emb_dimensions = [16,32,64,128]
+learning_rates = [0.0001,0.001,0.01]
+num_negatives = [1,5,10,20]
 
 
 
 
-for model in models:    
-    for emb_dimension in emb_dimensions:        
-        for learning_rate in learning_rates:            
-            for num_negative in num_negatives:
-            
-                # Define the model configuration
-                model_kwargs = {'embedding_dim': emb_dimension}
+for seed in seeds:
+    for model in models:    
+        for emb_dimension in emb_dimensions:        
+            for learning_rate in learning_rates:            
+                for num_negative in num_negatives:
+                
+                    # Define the model configuration
+                    model_kwargs = {'embedding_dim': emb_dimension}
 
-                # Define the training configuration
-                training_kwargs = {'batch_size':32,'num_epochs': epochs, 'use_tqdm_batch':False}
+                    # Define the training configuration
+                    training_kwargs = {'batch_size':32,'num_epochs': epochs, 'use_tqdm_batch':False}
 
-                # Define the optimizer configuration
-                optimizer_kwargs = {'lr':learning_rate}
+                    # Define the optimizer configuration
+                    optimizer_kwargs = {'lr':learning_rate}
 
-                # Define the evaluator configuration
-                evaluation_kwargs= {'batch_size':32,'use_tqdm':False}
+                    # Define the evaluator configuration
+                    evaluation_kwargs= {'batch_size':32,'use_tqdm':False}
 
-                # Define negative sampler evaluation
-                negative_sampler_kwargs = {'num_negs_per_pos':num_negative}
-
-
-                # Define the pipeline
-                pipe = pipeline(
-                    model = model,
-                    training = training,
-                    testing = testing,
-                    validation = validation,
-                    training_loop = 'slcwa',
-                    model_kwargs = model_kwargs,
-                    training_kwargs = training_kwargs,
-                    optimizer_kwargs = optimizer_kwargs,
-                    evaluation_kwargs = evaluation_kwargs,
-                    negative_sampler_kwargs = negative_sampler_kwargs,
-                    evaluation_relation_whitelist = evaluation_relation_whitelist if filtered_evaluation else None,
-                    metric = 'hits@3',
-                    negative_sampler = 'bernoulli',
-                    stopper = 'early',
-                    device = 'gpu',
-                    random_seed=1998
-                )
+                    # Define negative sampler evaluation
+                    negative_sampler_kwargs = {'num_negs_per_pos':num_negative}
 
 
-                parameters = {'model':model,'emb_dim':emb_dimension,'learning_rate':learning_rate,
-                              'negative_per_pos':num_negative,'n_epochs':epochs, 'filtered_eval':filtered_evaluation
-                              }
-                test_results = pipe.metric_results.to_df()
+                    # Define the pipeline
+                    pipe = pipeline(
+                        model = model,
+                        training = training,
+                        testing = testing,
+                        validation = validation,
+                        training_loop = 'slcwa',
+                        model_kwargs = model_kwargs,
+                        training_kwargs = training_kwargs,
+                        optimizer_kwargs = optimizer_kwargs,
+                        evaluation_kwargs = evaluation_kwargs,
+                        negative_sampler_kwargs = negative_sampler_kwargs,
+                        evaluation_relation_whitelist = evaluation_relation_whitelist if filtered_evaluation else None,
+                        metric = 'hits@3',
+                        negative_sampler = 'bernoulli',
+                        stopper = 'early',
+                        device = 'gpu',
+                        random_seed=seed
+                    )
 
 
-                name = model + '_'+str(emb_dimension) + '_' + str(learning_rate) + '_' + str(num_negative)+'_'+ str(epochs) + '_' + str(filtered_evaluation)
-                torch.save([parameters,test_results,pipe.losses],'./results/'+name+'.pkl')
-            
+                    parameters = {'model':model,'emb_dim':emb_dimension,'learning_rate':learning_rate,
+                                'negative_per_pos':num_negative,'n_epochs':epochs, 'filtered_eval':filtered_evaluation
+                                }
+                    test_results = pipe.metric_results.to_df()
+
+
+                    name = model + '_'+str(emb_dimension) + '_' + str(learning_rate) + '_' + str(num_negative)+'_'+ str(epochs) + '_' + str(filtered_evaluation)
+                    torch.save([parameters,test_results,pipe.losses],'./results/'+name+'.pkl')
+
+
+
+
+training_inv = TriplesFactory.from_labeled_triples(training.triples, create_inverse_triples=True)
+validation_inv = TriplesFactory.from_labeled_triples(validation.triples, create_inverse_triples=True)
+
+models = ['CompGCN']
+for seed in seeds:
+    for model in models:    
+        for emb_dimension in emb_dimensions:        
+            for learning_rate in learning_rates:            
+                for num_negative in num_negatives:
+                
+                    # Define the model configuration
+                    model_kwargs = {'embedding_dim': emb_dimension}
+
+                    # Define the training configuration
+                    training_kwargs = {'batch_size':32,'num_epochs': epochs, 'use_tqdm_batch':False}
+
+                    # Define the optimizer configuration
+                    optimizer_kwargs = {'lr':learning_rate}
+
+                    # Define the evaluator configuration
+                    evaluation_kwargs= {'batch_size':32,'use_tqdm':False}
+
+                    # Define negative sampler evaluation
+                    negative_sampler_kwargs = {'num_negs_per_pos':num_negative}
+
+
+                    # Define the pipeline
+                    pipe = pipeline(
+                        model = model,
+                        training = training_inv,
+                        testing = testing,
+                        validation = validation_inv,
+                        training_loop = 'slcwa',
+                        model_kwargs = model_kwargs,
+                        training_kwargs = training_kwargs,
+                        optimizer_kwargs = optimizer_kwargs,
+                        evaluation_kwargs = evaluation_kwargs,
+                        negative_sampler_kwargs = negative_sampler_kwargs,
+                        evaluation_relation_whitelist = evaluation_relation_whitelist if filtered_evaluation else None,
+                        metric = 'hits@3',
+                        negative_sampler = 'bernoulli',
+                        stopper = 'early',
+                        device = 'gpu',
+                        random_seed=seed
+                    )
+
+
+                    parameters = {'model':model,'emb_dim':emb_dimension,'learning_rate':learning_rate,
+                                'negative_per_pos':num_negative,'n_epochs':epochs, 'filtered_eval':filtered_evaluation
+                                }
+                    test_results = pipe.metric_results.to_df()
+
+
+                    name = model + '_'+str(emb_dimension) + '_' + str(learning_rate) + '_' + str(num_negative)+'_'+ str(epochs) + '_' + str(filtered_evaluation)
+                    torch.save([parameters,test_results,pipe.losses],'./results/'+name+'.pkl')
+
+
